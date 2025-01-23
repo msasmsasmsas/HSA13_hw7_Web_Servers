c54e72c13305268237e0c13ea8eb0cc604f66113
HSA13_hw4_Stress_Testing_Approaches_and_tools <msas@ukr.net>
2025-01-23 22:53:44 +0200

    1

---
+++ tests/scripts/purge_image.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+http PURGE http://web-proxy/purge/images/AI.png
+#curl -v -X PURGE http://web-proxy/purge/images/img1.jpg
---
+++ tests/scripts/test_web.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Test image URL (using web service name since we're in Docker network)
+IMAGE_URL="http://web/images/AI.png"
+
+# echo "First request:"
+# curl -X GET -I "$IMAGE_URL"
+
+# echo -e "\nSecond request:"
+# curl -X GET -I "$IMAGE_URL"
+
+# echo -e "\nThird request:"
+# curl -X GET -I "$IMAGE_URL"
+
+echo "First request:"
+http -h GET "$IMAGE_URL"
+
+echo -e "\nSecond request:"
+http -h GET "$IMAGE_URL"
+
+echo -e "\nThird request:"
+http -h GET "$IMAGE_URL"
