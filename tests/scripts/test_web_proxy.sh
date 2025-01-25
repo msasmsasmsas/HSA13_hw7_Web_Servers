2e3715a1a47a173be2d6007abe1b75d7587b8664
HSA13_hw4_Stress_Testing_Approaches_and_tools <msas@ukr.net>
2025-01-25 22:53:49 +0200

    1

--- tests/scripts/test_web_proxy.sh
+++ tests/scripts/test_web_proxy.sh
@@ -3,15 +3,6 @@
 # Test image URL (using web service name since we're in Docker network)
 IMAGE_URL="http://web-proxy/images/AI.png"
 
-# echo "First request:"
-# curl -X GET -I "$IMAGE_URL"
-
-# echo -e "\nSecond request:"
-# curl -X GET -I "$IMAGE_URL"
-
-# echo -e "\nThird request:"
-# curl -X GET -I "$IMAGE_URL"
-
 echo "First request:"
 http -h GET "$IMAGE_URL"
 
@@ -20,3 +11,12 @@ http -h GET "$IMAGE_URL"
 
 echo -e "\nThird request:"
 http -h GET "$IMAGE_URL"
+
+# echo "First request:"
+# curl -X GET -I "$IMAGE_URL"
+
+# echo -e "\nSecond request:"
+# curl -X GET -I "$IMAGE_URL"
+
+# echo -e "\nThird request:"
+# curl -X GET -I "$IMAGE_URL"
